32dfc3cd95fa38570a677b29729744130e17c2df
Piranha900 <40383231+Piranha900@users.noreply.github.com>
2022-05-18 23:11:41 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import twint
 
 c = twint.Config()
 
-c.Search = ['Taylor Swift']       # topic
-c.Limit = 500      # number of Tweets to scrape
-c.Store_csv = True       # store tweets in a csv file
-c.Output = "taylor_swift_tweets.csv"     # path to csv file
+c.Username = "elonmusk"
+#c.Custom["tweet"] = ["user_id", "created_at", "name", "username", "tweet", "language", "mentions", "urls", "photos", "", "replies_count" ]
+c.Since = "2022-05-03"
+c.Until = "2022-05-05"
+c.Limit = 1 # number of Tweets to scrape
+#c.Limit = 1000 # number of Tweets to scrape
+c.Store_json = True       # store tweets in a csv file
+c.Lang = "en"
+c.Output = "myresearch.json"     # path to json file
 
 twint.run.Search(c)
